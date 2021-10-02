fbfe2851162052dbd9328aee83ca8be8fa720382
Reynaldo Guzman <reynaldito@gmail.com>
2021-10-02 14:14:59 -0400

    dev: Added semantic-release stuff

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@ import setuptools
 
 setuptools.setup(
     name="truck_delivery_rayus007",
-    version="0.0.3",
     author="Reynaldo Guzman",
     description="An application to manage trucks",
     install_requires=['flask']
